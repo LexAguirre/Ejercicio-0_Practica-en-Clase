3d02c1bf271811345f59b1d9f2c1f5a7a71ca106
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2021-08-27 15:42:52 -0500

    Implemented better HTML

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ let classDado = new dado();
 let trabajo = new pagoTrabajo();
 let calculos = new Rectangulo(10, 20);
 
-console.log("Clase del Martes 17 de Agosto");
-console.log("Ejercicios con Sueldos");
+console.log("-------Clase del Martes 17 de Agosto-------");
+console.log("-------Ejercicios con Sueldos-------");
 console.log(trabajo.sueldo());
 
-console.log("Practica del Jueves 19 de Agosto");
-console.log("Ejercicios de Rectangulos");
+console.log("-------Practica del Jueves 19 de Agosto-------");
+console.log("-------Ejercicios de Rectangulos-------");
 console.log(calculos.obtenerArea());
 console.log(calculos.obtenerPerimetro());
 
-console.log("Clase del Martes 24 de Agosto");
-console.log("Ejercicios con Dados");
+console.log("-------Clase del Martes 24 de Agosto-------");
+console.log("-------Ejercicios con Dados-------");
 console.log(classDado.count());
 //console.log(classDado.jugar());
 //onsole.log(classDado.demostracion());
